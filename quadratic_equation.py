4f03030751c85bd646fc8ba53a63faf135a8f351
John2013 <NaWasington@yandex.ru>
2018-01-12 22:14:41 +0300

    упростил функцию убрал вложенное условие

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -7,7 +7,7 @@ def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
     if discriminant >= 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
-        if discriminant > 0:
-            root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant > 0:
+        root2 = (-b + sqrt(discriminant)) / (2 * a)
 
     return root1, root2
