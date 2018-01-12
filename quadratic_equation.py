1c8e41c2e392e8919ae267b25cbad8367a132038
John2013 <NaWasington@yandex.ru>
2018-01-12 22:17:21 +0300

    убрал строку, которая не понравилась проверяющему

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -2,12 +2,13 @@ from math import sqrt
 
 
 def get_roots(a, b, c):
-    root1, root2 = None, None
-
     discriminant = b ** 2 - 4 * a * c
     if discriminant >= 0:
         root1 = (-b - sqrt(discriminant)) / (2 * a)
+        root2 = None
     if discriminant > 0:
         root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant < 0:
+        root1, root2 = None, None
 
     return root1, root2
