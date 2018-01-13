c62495be34e23346acf8b8a2b1a2560f1c016e43
John2013 <NaWasington@yandex.ru>
2018-01-14 01:17:18 +0300

    до сих пор не втыкаю, чего от меня хотят

--- quadratic_equation.py
+++ quadratic_equation.py
@@ -3,13 +3,17 @@ from math import sqrt
 
 def get_roots(a, b, c):
     discriminant = b ** 2 - 4 * a * c
-    if discriminant == 0:
-        root1 = -b / (2 * a)
-        root2 = None
-    elif discriminant > 0:
-        root1 = (-b - sqrt(discriminant)) / (2 * a)
-        root2 = (-b + sqrt(discriminant)) / (2 * a)
+    if discriminant >= 0:
+        sqrt_discriminant = sqrt(discriminant)
+        two_a = 2 * a
+
+        root1 = (-b - sqrt_discriminant) / two_a
+        if discriminant != 0:
+            root2 = (-b + sqrt_discriminant) / two_a
+        else:
+            root2 = None
     else:
         root1, root2 = None, None
 
     return root1, root2
+
